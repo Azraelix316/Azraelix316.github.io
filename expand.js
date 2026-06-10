c7c4032669ee9da10a55bb5cf1be3164a501d7e1
Azraelix316 <jaredj13310@gmail.com>
2026-06-10 09:34:03 +0800

    Remove all old files

--- expand.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('click', function(event) {
-    const clickedElement = event.target;
-    const parentDiv = clickedElement.closest('div');
-    const nameOfClass = parentDiv.classList[0];
-    document.getElementsByClassName(nameOfClass)[1].classList.toggle("focus");
-  });
