d96ea48f2b8e02bd4422039145d952d805dca646
Azraelix316 <jaredj13310@gmail.com>
2026-08-05 22:20:57 +0900

    Genuinely too tired

--- vite.config.js
+++ vite.config.js
@@ -12,13 +12,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: 'terser',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom']
-        }
-      }
-    }
+    minify: 'terser'
   }
 })
